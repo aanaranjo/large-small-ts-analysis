b0dbfcdcdcf6fffe40c0cf7a3503c5263313df95
Andre Naranjo <aanaranjo@ufl.edu>
2016-11-18 13:21:15 -0500

    Merging original house elf CSV with changed name of house elf CSV

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,26 @@
 #This script is meant to analyze the DNA of house elves in my living room
-houseelf_earlength_dna <- read.csv("houseelf-earlength-dna-data.csv")
+#Commit 1 was random
+houseelf_earlength_dna <- read.csv("houseelf-earlength-dna-data-1.csv")
+#Commit 2 was to add CSV
+
+
+#Pushing Changes
+
+
+
+
+
+
+
+
+#Get GC Content
+library(stringr)
+library(ggplot)
+
+Get_GC_Content <- function(houseelf_earlength_dna){
+  seq_upper <- str_to_upper(houseelf_earlength_dna)
+  Gs <- str_count(seq_upper, "G")
+  Cs <- str_count(seq_upper, "C")
+  gc_content <- (Gs +Cs) / str_length(seq_upper) *100
+  return(gc_content)
+}
